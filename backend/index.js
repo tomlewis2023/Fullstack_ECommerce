3222eace799dd75220c33540aecef599762d0f53
tomlewis2023 <tomlewis2005@gmail.com>
2024-09-08 13:27:02 +0530

    update cors

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,8 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./config/db");
-const router = require('./routes/index')
-const cookieParser = require('cookie-parser')
-
+const router = require("./routes/index");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 // app.use(cors({
@@ -13,35 +12,33 @@ const app = express();
 // }));
 
 const allowedOrigins = [
-  'https://fullstack-e-commerce-u1vh.vercel.app',
-  'https://fullstack-e-commerce-u1vh-n1dajpv64-tom-lewis-projects-57b7522f.vercel.app' // Add all frontend origins
+  process.env.FRONTEND_URL,
   // Add other allowed origins if needed
 ];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        // Allow requests with no origin (e.g., server-to-server)
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) { // Allow requests with no origin (e.g., server-to-server)
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+app.options("*", cors());
 
+app.use(express.json());
+app.use(cookieParser());
 
+app.use("/api", router);
 
-
-app.use(express.json())
-app.use(cookieParser())
-
-app.use('/api',router)
-
-
-
-const PORT = 8080 || process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
